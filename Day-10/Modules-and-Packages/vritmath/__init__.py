0af8636804f15cf0a6c5a5b22a70b2a7238096cb
ShaileshAdhikari <shailesh.adhikari98@gmail.com>
2024-02-29 12:17:32 +0545

    Update Day-11 : Exception handling

---
+++ Day-10/Modules-and-Packages/vritmath/__init__.py
@@ -0,0 +1,5 @@
+from .add_two import Sum as add_two_numbers
+
+from .multiply import multiply as multiply_two_numbers
+
+from .divide import divide as divide_two_numbers
---
+++ Day-10/Modules-and-Packages/vritmath/add_two.py
@@ -0,0 +1,7 @@
+class Sum:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+    def add(self):
+        return self.x + self.y
